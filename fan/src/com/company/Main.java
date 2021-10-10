e0c9909d8b6c246944d0ddf7979217c89fc4582c
Phạm Thị Lan Anh <90228386+LananhHoadanhdanh@users.noreply.github.com>
2021-10-11 00:33:54 +0700

    class fan

--- fan/src/com/company/Main.java
+++ fan/src/com/company/Main.java
@@ -12,12 +12,12 @@ public class Main {
         fan1.setRadius(10);
         fan1.setColor("Yellow");
         fan1.setOn(true);
-        System.out.println(fan1.toString());
+        System.out.println(fan1);
 
         Fan fan2 = new Fan();
         fan2.setSpeed(MEDIUM);
         fan2.setRadius(5);
         fan2.setColor("Blue");
-        System.out.println(fan2.toString());
+        System.out.println(fan2);
     }
 }
